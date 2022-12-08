ce0c8afb7296b046c22f65d7eed66af1a1bb2e35
Ahmedshukr <ahmetoshukr@gmail.com>
2022-12-08 07:36:38 +0000

    data wranling is done

--- penguins.R
+++ penguins.R
@@ -2,11 +2,18 @@ install.packages("tidyverse")
 install.packages("palmerpenguins")
 
 library(ggplot2)
+library(tidyverse)
 library(dplyr)
 library(palmerpenguins)
 
 data("penguins")
 View(penguins)
+
+# Assessing the dataset
+head(penguins)
+tail(penguins)
+
+colnames(penguins)
 
 # let us asses the structure of the dataset
 str(penguins)
@@ -15,7 +22,33 @@ str(penguins)
 glimpse(penguins)
 
 #count the penguin's species in the dataset
-count(penguins, species)
+penguins %>%
+  count(species)
+
+#summary of the dataset
+summary(penguins)
+
+#looking for duplicated rows
+n_distinct(penguins)
+
+# checking correlation between numeric variables
+penguins %>% 
+  drop_na() %>% 
+  select_if(is.numeric) %>% 
+  cor()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
